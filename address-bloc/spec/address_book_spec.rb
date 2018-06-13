974fe456e6c31d2d7befff61fea8dbb621f06f0a
Ann Murray <ann@amurraypr.com>
2018-06-12 22:38:28 -0700

    Assignment-Model completed

--- address-bloc/spec/address_book_spec.rb
+++ address-bloc/spec/address_book_spec.rb
@@ -18,12 +18,48 @@ RSpec.describe AddressBook do
      end
   end
 
-describe "#add_entry" do
+  describe "#remove_entry" do
+    it "removes an entry using the name, phone_number, and email" do
+      book = AddressBook.new
+      book.add_entry('Ann Murray', '555.555.5555', 'ann@amurraypr.com')
+
+      name = "Ada Lovelace"
+      phone_number = "010.012.1815"
+      email = "augusta.king@lovelace.com"
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq 2
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq 1
+expect(book.entries.first.name).to eq("Ann Murray")
+      end
+  end
+
+# describe "#remove_entry" do
+#   it "removes an entry using the name, phone_number, and email" do
+#     book = AddressBook.new
+#     book.add_entry('Ann Murray', '222.222.2222', 'ann@amurraypr.com')
+#
+#     name = 'Mickey Mouse'
+#     phone_number = '666.666.6666'
+#     email_address = 'mickey.mouse@disney.com'
+#     book.add_entry(name, phone_number, email_address)
+#
+#     expect(book.entries.size).to eq(1)
+#     book.remove_entry(name, phone_number, email_address)
+#     expect(book.entries.size).to eq(1)
+#     expect(book.entries.first.name).to eq('Ann Murray')
+#
+# end
+
+  describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
        expect(book.entries.size).to eq(1)
+
+
      end
 
      it "adds the correct information to entries" do
