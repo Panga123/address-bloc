7253a46e96e7e1ccf5eaa8290ac08af3b3bee5f0
Ann Murray <ann@amurraypr.com>
2018-06-14 21:54:50 -0700

    fixing git

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,7 +14,8 @@ require_relative '../models/address_book'
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View specific entry!"
+     puts "6 - Exit!"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -37,7 +38,12 @@ require_relative '../models/address_book'
              system "clear"
              read_csv
              main_menu
-           when 5
+          when 5
+             system "clear"
+             entry_n_submenu
+             main_menu
+
+           when 6
              puts "Good-bye!"
              # #8
              exit(0)
@@ -48,6 +54,22 @@ require_relative '../models/address_book'
              main_menu
          end
        end
+
+       def entry_n_submenu
+         print "Entry number to view: "
+         selection = gets.chomp.to_i
+
+         if selection < @address_book.entries.count
+           puts @address_book.entries[selection]
+           puts "Press enter to return to the main menu"
+           gets.chomp
+           system "clear"
+
+         else
+           puts "#{selection} is not a valid input!"
+           entry_n_submenu
+         end
+      end
 
        # #10
        def view_all_entries
