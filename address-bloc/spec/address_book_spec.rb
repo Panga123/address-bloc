f7a9d829bb7f3c1f97dd151873184e645e8f2f59
Ann Murray <ann@amurraypr.com>
2018-06-19 20:51:19 -0700

    assignment csv done

--- address-bloc/spec/address_book_spec.rb
+++ address-bloc/spec/address_book_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe AddressBook do
       expect(book_size).to eq 5
     end
 
-  end
+  #end
 
   # #4
   it "imports the 1st entry" do
@@ -102,5 +102,45 @@ RSpec.describe AddressBook do
   end
 end
 
-#end
-#end
+describe "#import_from_csv2" do
+  it "imports the correct number of entries from 2" do
+    # #3
+    book.import_from_csv("entries_2.csv")
+    #  book_size = book.entries.size
+    # Check the size of the entries in AddressBook
+    #expect(book_size).to eq 3
+    expect(book.entries.size).to eq 3
+  end
+
+  # context "importing from entries_2.csv" do
+  #   it "imports the correct number of entries" do
+  #     book.import_from_csv("entries_2.csv")
+  #
+  #     expect(book.entries.size).to eq 3
+  #   end
+
+  it "imports the 1st entry" do
+    book.import_from_csv("entries_2.csv")
+    # Check the first entry
+    entry_one = book.entries[0]
+
+    # #5
+    check_entry(entry_one, "Ann", "555-555-0000", "ann@blocmail.com")
+  end
+
+  it "imports the 2nd entry" do
+    book.import_from_csv("entries_2.csv")
+    # Check the second entry
+    entry_two = book.entries[1]
+    check_entry(entry_two, "Ben", "555-555-1111", "ben@blocmail.com")
+  end
+
+  it "imports the 3rd entry" do
+    book.import_from_csv("entries_2.csv")
+    # Check the third entry
+    entry_three = book.entries[2]
+    check_entry(entry_three, "Jane", "555-555-2222", "jane@blocmail.com")
+  end
+
+end
+end
